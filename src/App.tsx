d2ac5386b17b1b336cef0843411bcb0900d75e36
Thedub2001 <thedub2001@hotmail.com>
2025-09-02 20:37:03 +0200

    integration test with molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+// App.tsx
+import React from "react";
+import { create, Workbench, molecule } from "@dtinsight/molecule";
+import { IContributeType, IExtension } from '@dtinsight/molecule/esm/model';
+import { IExtensionService } from '@dtinsight/molecule/esm/services';
+import "@dtinsight/molecule/esm/style/mo.css";
 
-import { create, Workbench } from '@dtinsight/molecule';
-import '@dtinsight/molecule/esm/style/mo.css';
+import GraphApp from "./GraphApp"; // ← ton app actuelle déplacée ici (SettingsPanel+GraphWrapper+InfoPanel)
 
+console.log("[molecule][init] starting App.tsx");
+
+// --- Extension Molecule ---
+// On crée un panneau central pour afficher GraphApp
+const GraphExtension: IExtension = {
+  id: "graph.extension",
+  name: "Graph Extension",
+  activate(extensionCtx: IExtensionService) {
+    console.log("[molecule][activate] GraphExtension activée");
+
+    molecule.panel.add({
+      id: "graph.panel",
+      name: "Graph Panel",
+      renderPane: () => {
+        console.log("[molecule][render] GraphPanel rendu");
+        return <GraphApp />;
+      },
+    });
+
+    // Option : sélectionner ce panel au démarrage
+    molecule.panel.setActive("graph.panel");
+  },
+  dispose(extensionCtx: IExtensionService) {
+    console.log("[molecule][dispose] GraphExtension supprimée");
+  },
+};
+
+// --- Création Molecule ---
 const moInstance = create({
-  extensions: [],
+  extensions: [GraphExtension],
 });
 
-const App = () => moInstance.render(<Workbench />);
+const App = () => {
+  console.log("[molecule][render] App rendu");
+  return moInstance.render(<Workbench />);
+};
 
-
-export default App
+export default App;
---
+++ src/GraphApp.tsx
@@ -0,0 +1,89 @@
+// GraphApp.tsx
+import React from "react";
+import SettingsPanel from "./components/SettingsPanel";
+import InfoPanel from "./components/InfoPanel";
+import GraphWrapper from "./components/GraphWrapper";
+import { useNodes } from "./hooks/useNodes";
+import { useLinks } from "./hooks/useLinks";
+import { useGraphSelection } from "./hooks/useGraphSelection";
+import { useVisualLinks } from "./hooks/useVisualLinks";
+import { useGraphDataSync } from "./hooks/useGraphDataSync";
+import { useGraphInitialFetch } from "./hooks/useGraphInitialFetch";
+import useCameraTracker from "./hooks/useCameraTracker";
+import { useNodeLabelGenerator } from "./hooks/useNodeLabelGenerator";
+import { addChildNodeHandler, deleteNodeRecursive } from "./utils/nodeHandlers";
+import useLabelSprite from "./components/LabelSprite";
+import type { ForceGraphMethods } from "react-force-graph-3d";
+import type { NodeType, LinkType } from "./types/graph";
+
+function GraphApp() {
+  console.log("[graph][render] GraphApp rendu");
+
+  const fgRef = React.useRef<ForceGraphMethods<NodeType, LinkType> | null>(null);
+
+  const { nodes, fetchGraphData, addNode, deleteNode } = useNodes();
+  const { links, fetchLinks, addLink, deleteLink } = useLinks();
+  const { visualLinks, fetchVisualLinks, addVisualLink, removeVisualLink } = useVisualLinks();
+  const {
+    selectedNodes,
+    selectedLinks,
+    selectedNodeObjects,
+    selectedLinkObjects,
+    setSelectedNodes,
+    setSelectedLinks,
+    getLinkId,
+    onNodeClick,
+    onLinkClick,
+  } = useGraphSelection(nodes, links);
+
+  const graphData = useGraphDataSync(nodes, links);
+  const cameraPos = useCameraTracker(fgRef);
+  const generateTextLabel = useNodeLabelGenerator();
+  const nodeThreeObject = useLabelSprite({ cameraPos, generateTextLabel });
+
+  useGraphInitialFetch(fetchGraphData, fetchLinks, fetchVisualLinks);
+
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <SettingsPanel />
+      <GraphWrapper
+        fgRef={fgRef}
+        graphData={graphData}
+        selectedNodes={selectedNodes}
+        selectedLinks={selectedLinks}
+        setSelectedLinks={setSelectedLinks}
+        getLinkId={getLinkId}
+        nodeThreeObject={nodeThreeObject}
+        onNodeClick={onNodeClick}
+        onLinkClick={onLinkClick}
+        visualLinks={visualLinks}
+      />
+      <InfoPanel
+        selectedNodes={selectedNodeObjects}
+        selectedLinks={selectedLinkObjects}
+        nodes={nodes}
+        links={links}
+        onClose={() => {
+          setSelectedNodes(new Set());
+          setSelectedLinks(new Set());
+        }}
+        onCreateChildNode={async (parentId) => {
+          await addChildNodeHandler(parentId, nodes, addNode, addLink);
+        }}
+        onDeleteNode={async (nodeId) => {
+          await deleteNodeRecursive(
+            nodeId,
+            nodes,
+            links,
+            deleteNode,
+            deleteLink,
+            visualLinks,
+            removeVisualLink
+          );
+        }}
+      />
+    </div>
+  );
+}
+
+export default GraphApp;
